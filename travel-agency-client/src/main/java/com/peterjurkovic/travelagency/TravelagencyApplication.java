8f1e84f02218bddbfbb98f80f4de1f515a839630
peterjurkovic <petojurkovic@gmail.com>
2017-12-20 13:40:44 +0000

    Added skeleton of the project

---
+++ travel-agency-client/src/main/java/com/peterjurkovic/travelagency/TravelagencyApplication.java
@@ -0,0 +1,12 @@
+package com.peterjurkovic.travelagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelagencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelagencyApplication.class, args);
+	}
+}
